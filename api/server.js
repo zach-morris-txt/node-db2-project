eecb2908fc8083948c28856de09ee4df88187914
unknown <zach-morris@lambdastudents.com>
2021-07-13 22:31:16 -0400

    Debugger1

--- api/server.js
+++ api/server.js
@@ -7,5 +7,15 @@ server.use(express.json())
 
 // DO YOUR MAGIC
 server.use('/api/cars', carsRouter)
-
+server.use('*', (req, res, next) => {
+    next({
+        status: 404,
+        message: 'page not found'
+    })
+})
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
 module.exports = server
