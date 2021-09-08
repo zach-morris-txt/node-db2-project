97bdde9ef3c4b7797a6b106b2c877d7e8a43bd6e
unknown <zach-morris@lambdastudents.com>
2021-09-07 20:56:04 -0400

    Timeout Tests

--- api/server.js
+++ api/server.js
@@ -1,7 +1,21 @@
 const express = require("express")
+const carsRouter = require('./cars/cars-router')
+
 
 const server = express()
+server.use(express.json())
 
 // DO YOUR MAGIC
-
+server.use('/api/cars', carsRouter)
+server.use('*', (req, res, next) => {
+    next({
+        status: 404,
+        message: 'page not found'
+    })
+})
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
 module.exports = server
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,16 @@
 // DO YOUR MAGIC
+exports.up = function(knex) {
+    return knex.schema.createTable('cars', table => {
+        table.increments('id')
+
+        table.text('vin', 17).unique().notNullable()
+        table.text('make', 128).notNullable()
+        table.text('model', 128).notNullable()
+        table.integer('mileage', 128).unsigned().notNullable()
+        table.text('title', 128)
+        table.text('transmission', 128) 
+    })
+}
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars')
+}
